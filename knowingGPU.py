b9f030783e678601cb86101ffb877890173428fa
Ha Ngoc Linh <linhhn13@fe.edu.vn>
2022-12-17 17:58:59 +0700

    not sure it is correct or not

---
+++ knowingGPU.py
@@ -0,0 +1,10 @@
+from numba import cuda
+from numba import numba
+
+gpus = cuda.gpus.lst
+num_cores = numba.config.NUMBA_DEFAULT_NUM_THREADS
+for gpu in gpus:
+    with gpu:
+        meminfo = cuda.current_context().get_memory_info()
+        print("Device name: %s, free: %s bytes, total, %s bytes" % (gpu, meminfo[0], meminfo[1]))
+        print('Number of cores: ' + str(num_cores))
